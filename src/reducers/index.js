938c5cf69959200f478bbd49a5936a7cfe4bc14e
Casey The Great <casey@caseypitman.com>
2020-11-24 10:22:04 -0600

    Created songsReducer.

---
+++ src/reducers/index.js
@@ -0,0 +1,32 @@
+//Reducers
+
+//List of songs
+export const songsReducer = () => {
+  return [
+    {
+      title: "There, There",
+      artist: "Radiohead",
+      duration: "5:23",
+    },
+    {
+      title: "Via Chicago",
+      artist: "Wilco",
+      duration: "5:33",
+    },
+    {
+      title: "I Believe",
+      artist: "R.E.M.",
+      duration: "3:49",
+    },
+    {
+      title: "The Boy with the Arab Strap",
+      artist: "Belle & Sebastian",
+      duration: "5:14",
+    },
+    {
+      title: "Exile",
+      artist: "Taylor Swift",
+      duration: "4:45",
+    },
+  ];
+};
